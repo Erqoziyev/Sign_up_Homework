c027b107238f90dad623a13f64ea25988cd8c1c7
99891 <99891@DESKTOP-LRVSC1R>
2023-06-18 00:45:59 +0500

    Add project files.

---
+++ Sign_up_Homework/Constans/db_constans.cs
@@ -0,0 +1,11 @@
+﻿namespace Sign_up_Homework.Constans;
+
+public class db_constans
+{
+    public const string DB_CONNECTION_STRING =
+                        "Host=localhost;" +
+                        "Port=5432;" +
+                        "Database=Sign_up_db;" +
+                        "User Id=postgres;" +
+                        "Password=0809;";
+}
---
+++ Sign_up_Homework/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using Npgsql;
+using Sign_up_Homework.Constans;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Sign_up_Homework
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async  void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            await using (var connection = new NpgsqlConnection(db_constans.DB_CONNECTION_STRING))
+            {
+                await connection.OpenAsync();
+
+                string email = tbEmail.Text;
+                string password = tbPassword.Password;
+
+                string query = $"select *from users where id = '1'";
+
+
+                await using (var command = new NpgsqlCommand(query, connection))
+                {
+                    await using (var reader = await command.ExecuteReaderAsync())
+                    {
+                        while (await reader.ReadAsync())
+                        {
+                            if (email == reader.GetString(1) && password == reader.GetString(2))
+                            {
+                                MessageBox.Show("Successful");
+                            }
+                            else
+                                MessageBox.Show("Eror");
+                        }
+                        await reader.CloseAsync();
+                    }
+                }
+                await connection.CloseAsync();
+            }
+        }
+    }
+}
